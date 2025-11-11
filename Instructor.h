3569ff8cccc93336c3db25fd33114875690928aa
MohamedShabrawy16003169 <mohamednwnw.2016@gmail.com>
2025-11-11 09:21:52 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,16 +4,34 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+    Instructor() : Person(), department(""), experienceYears(0) {}
+    Instructor(string n, int i, string d, int e)
+        : Person(n, i), department(d), experienceYears(e) {}
 
+    string getDepartment() { 
+      return department; 
+    }
+    int getExperienceYears() { 
+      return experienceYears;
+    }
 
+    void setDepartment( string& d) { 
+      department = d; 
+    }
+    void setExperienceYears(int e) { 
+      experienceYears = e;
+    }
 
-
-
-
-
-
-
-
-
+    void display()  override {
+        cout << "Name: " << getName() << endl;
+        cout << "Department: " << department << endl;
+        cout << "Experience: " << experienceYears << endl;
+    }
+};
 #endif
