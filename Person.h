842c1f5a8c8f663742b3e28232f044d7e039f955
MohamedShabrawy16003169 <mohamednwnw.2016@gmail.com>
2025-11-11 09:33:47 +0200

    Refactor Person class header for better structure

--- Person.h
+++ Person.h
@@ -5,30 +5,28 @@
 using namespace std;
 
 //#write Person class here
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
 class Person {
 private:
     string name;
     int id;
 
 public:
-    Person() : name(""), id(0) {}
-    Person(string n, int i) : name(n), id(i) {}
+    Person();
+    Person(string n, int i);
 
-    string getName(){ 
-      return name; 
-    }
-    int getId() { 
-      return id; 
-    }
+    string getName();
+    int getId();
 
-    void setName(string& n) { 
-      name = n; 
-    }
-    void setId(int i) { id = i; }
+    void setName(string& n);
+    void setId(int i);
 
-    virtual void display(){
-        cout << "Name: " << name << endl;
-        cout << "ID: " << id << endl;
-    }
+    virtual void display();
+    virtual ~Person();
 };
 #endif
