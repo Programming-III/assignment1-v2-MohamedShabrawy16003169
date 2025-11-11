1632e76dde8002bf6670c48096a9eba0700b8dcd
MohamedShabrawy16003169 <mohamednwnw.2016@gmail.com>
2025-11-11 09:13:49 +0200

    Implement Person class with basic functionality

--- Person.h
+++ Person.h
@@ -5,16 +5,30 @@
 using namespace std;
 
 //#write Person class here
+class Person {
+private:
+    string name;
+    int id;
 
+public:
+    Person() : name(""), id(0) {}
+    Person(string n, int i) : name(n), id(i) {}
 
+    string getName(){ 
+      return name; 
+    }
+    int getId() { 
+      return id; 
+    }
 
+    void setName(string& n) { 
+      name = n; 
+    }
+    void setId(int i) { id = i; }
 
-
-
-
-
-
-
-
-
+    virtual void display(){
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+    }
+};
 #endif
