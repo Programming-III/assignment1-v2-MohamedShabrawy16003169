494b906b76de97cfaa13df871316bf704a39c125
MohamedShabrawy16003169 <mohamednwnw.2016@gmail.com>
2025-11-11 09:19:35 +0200

    Implement Student class inheriting from Person

--- Student.h
+++ Student.h
@@ -6,6 +6,35 @@
 #using namespace std;
 //#write student class here
 
+class Student : public Person {
+private:
+    int year;
+    string major;
+
+public:
+    Student() : Person(), year(0), major("") {}
+    Student(string n, int i, int y, string m) : Person(n, i), year(y), major(m) {}
+
+    int getYear()  { 
+      return year; 
+    }
+    string getMajor(){ 
+      return major; 
+    }
+
+    void setYear(int y) { 
+      year = y; 
+    }
+    void setMajor(string& m) {
+      major = m;
+    }
+
+    void display()override {
+        cout << "Name: " << getName() << endl;
+        cout << "Year: " << year << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 
 
